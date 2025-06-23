253e431c06b732b436695625d38c4f75aa3f45e4
Joel Oliveira <joel80851@gmail.com>
2025-06-23 17:59:39 +0530

    implemented jwt authentication

---
+++ src/main/java/com/joel/app/security/service/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.joel.app.security.service;
+
+import com.joel.app.model.User;
+import com.joel.app.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with email: " + email));
+
+        return UserDetailsImpl.build(user);
+    }
+
+
+
+
+}
