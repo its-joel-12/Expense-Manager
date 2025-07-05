b5b3750b5e12cd750f6da31026d07762b54d426a
Joel Oliveira <joel80851@gmail.com>
2025-07-05 15:55:21 +0530

    some changes

--- src/main/java/com/joel/app/utils/AuthUtil.java
+++ src/main/java/com/joel/app/utils/AuthUtil.java
@@ -1,5 +1,6 @@
 package com.joel.app.utils;
 
+import com.joel.app.exception.ResourceNotFoundException;
 import com.joel.app.model.User;
 import com.joel.app.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,12 +15,12 @@ public class AuthUtil {
 
     public Long loggedInUserId(){
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        User user = userRepository.findByEmail(authentication.getName()).orElseThrow(() -> new RuntimeException("User not found"));
+        User user = userRepository.findByEmail(authentication.getName()).orElseThrow(() -> new ResourceNotFoundException("User not found"));
         return user.getUserId();
     }
 
     public User loggedInUser(){
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        return userRepository.findByEmail(authentication.getName()).orElseThrow(() -> new RuntimeException("User not found"));
+        return userRepository.findByEmail(authentication.getName()).orElseThrow(() -> new ResourceNotFoundException("User not found"));
     }
 }
